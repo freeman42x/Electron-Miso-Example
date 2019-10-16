e4d31e93e91ac141d05c1e9f8adc0baad938b981
Răzvan Flavius Panda <razvan.panda@gmail.com>
2019-10-16 19:53:52 +0100

    Electron + Miso + JSADDLE + HIE

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 with (import (builtins.fetchTarball {
-  url = "https://github.com/dmjio/miso/archive/39b9e26ff41d6aab3b9d13a9d102ac56017f6a1f.tar.gz";
-  sha256 = "1lwr35p9074b7wgz0jh4f2pjc7ls8isgzmn9xl86vb6cvsm035kf";
+  url = "https://github.com/dmjio/miso/archive/4e15d92cc4f37605fc398229695de4d903ef8a47.tar.gz";
+  sha256 = "0kg9lg18qdsnb6gw3jmnl49widzf5cy151bls7k09d5dxlm0myd8";
 }) {});
 with pkgs.haskell.packages;
-ghcjs.callCabal2nix "test" ./. {}
+ghc865.callCabal2nix "app" ./. { miso = ghc865.miso-jsaddle; }
